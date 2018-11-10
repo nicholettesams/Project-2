9baced199a4f9b3ad28b66de456df53f93ed6e8d
nmiesmer <nic.miesmer@segmint.com>
2018-11-10 13:13:45 -0500

    serve listening more project set up

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -14,14 +13,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/SOMEFILE.js");
+const routes = require("./app/routes/htmlRoutes.js");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
