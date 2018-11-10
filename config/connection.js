645a4040d011e708bf83209c5485d23dd453d14a
nmiesmer <nic.miesmer@segmint.com>
2018-11-10 10:52:14 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-var connection;
+let connection;
 // Jaws database
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL)
