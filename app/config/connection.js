99bf0585f8ccd163bbd7c95f5bad31ebb67b0a03
Sams <nicholettesams@msn.com>
2018-11-15 21:19:52 -0500

    Eli and Dustin helped me

--- app/config/connection.js
+++ app/config/connection.js
@@ -10,14 +10,16 @@ if (process.env.JAWSDB_URL){
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "zoo"
+    password: "root",
+    database: "zoomate"
 });
 
 }
 
 // Make connection.
 connection.connect(function(err) {
+
+
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
