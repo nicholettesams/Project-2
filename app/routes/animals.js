53eeb7a2aea29412280572fae730c955fe76c1d2
nmiesmer <nic.miesmer@segmint.com>
2018-11-16 21:18:59 -0500

    need to figure out post from body

--- app/routes/animals.js
+++ app/routes/animals.js
@@ -7,7 +7,7 @@ var connection = require("../config/connection.js");
 //all animals belonging to a zoo
 router.get("/api/animals/:zoo_id", function(req, res) {
 
-    const sql = "SELECT * FROM zoos WHERE id = "+ req.params.zoo_id;
+    const sql = "SELECT * FROM animals WHERE id = "+ req.params.zoo_id;
 
     connection.query(sql, function(err, results) {
         if (err) throw err;
@@ -21,29 +21,36 @@ router.get("/api/animals/:zoo_id", function(req, res) {
 });
 
 
-// //Display get all animals for an animal
-// router.get("/api/animals/:animal_id/profile", function(req, res) {
+//Display get all animals for an animal
+router.get("/api/animals/:animal_id/profile", function(req, res) {
+
+    const sql = "SELECT * FROM animals WHERE id = "+ req.params.animal_id;
+
+
+
+    connection.query(sql, function(err, results) {
+        if (err) throw err;
+
+        res.json(results)
+
+    });
+
+});
+
+
+//add an animal
+router.post("/api/animals", function(req, res) {
 //
-//
-//     connection.query(sql, function(err, results) {
-//         if (err) throw err;
-//
-//         res.json(results)
-//
-//     });
-//
-// });
-//
-// //add an animal
-// router.post("/api/animals/:animal_id", function(req, res) {
-//
-//
-//
-// });
-//
-// router.put("/api/animals/:animal_id", function(req, res) {
-//
-// });
+// const sql = "INSERT INTO animals\n" +
+//     "(animal_name, zoo_id, species_id, mom_id, dad_id, gender, matable, birth_date, image_URL)\n" +
+//     "values\n" +
+//     "       ('Mrs. Jumbo', 1, 1, 0, 0, 'F', TRUE, '2008-01-01', '');"
+
+});
+
+router.put("/api/animals/:animal_id", function(req, res) {
+
+});
 
 
 
