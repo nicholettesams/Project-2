54da0aef40e02daf565ba9b44b68a3a472633f00
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-17 13:03:17 -0500

    duplicate from mere gge fixed

--- app/public/assets/js/add_animal.js
+++ app/public/assets/js/add_animal.js
@@ -1,6 +1,5 @@
 
 $(function() {
-
     // Load zoo drop down
     $.ajax("/api/zoos", {
     type: "GET",
@@ -77,79 +76,7 @@ $(function() {
             var el = $('<option>')
             el.attr("table_id", 0)
             el.attr("selected")
-            el.text("Selct your animal")
-            $("#dad-dd").append(el);
-
-            for(i=0; i< res.length; i++){
-                var el = $('<option>')
-                el.attr("table_id", res[i].id)
-                el.attr("value", res[i].animal_name)
-                el.text(res[i].animal_name)
-                $("#dad-dd").append(el);
-
-            }
-        });
-
-
-$(function() {
-
-    // Load zoo drop down
-    $.ajax("/api/zoos", {
-    type: "GET",
-    }).then(
-    function(res) {
-
-        var el = $('<option>')
-        el.attr("table_id", 0)
-        el.attr("selected")
-        el.text("Select your zoo")
-        $("#zoo-dd").append(el); 
-
-        for(i=0; i< res.length; i++){
-            var el = $('<option>')
-            el.attr("table_id", res[i].id)
-            el.attr("value", res[i].zoo_name)
-            el.text(res[i].zoo_name)
-            $("#zoo-dd").append(el);  
-
-        }
-    });
-
-
-    // Load species drop down
-    $.ajax("/api/species", {
-        type: "GET",
-        }).then(
-        function(res) {
-    
-            var el = $('<option>')
-            el.attr("table_id", 0)
-            el.attr("selected")
-            el.text("Select your species")
-            $("#species-dd").append(el); 
-    
-            for(i=0; i< res.length; i++){
-                var el = $('<option>')
-                el.attr("table_id", res[i].id)
-                el.attr("value", res[i].species_name)
-                el.text(res[i].species_name)
-                $("#species-dd").append(el);  
-    
-            }
-        });
-
-
-    // Load mom and dad drop down
-    $.ajax("/api/animals", {
-        type: "GET",
-        }).then(
-        function(res) {
-
-            var el = $('<option>')
-            el.attr("table_id", 0)
-            el.attr("selected")
             el.text("Select your animal")
-            $("#mom-dd").append(el); 
             $("#dad-dd").append(el);
 
             for(i=0; i< res.length; i++){
@@ -157,9 +84,15 @@ $(function() {
                 el.attr("table_id", res[i].id)
                 el.attr("value", res[i].animal_name)
                 el.text(res[i].animal_name)
-                $("#mom-dd").append(el);  
                 $("#dad-dd").append(el);
 
             }
         });
+
+
+    
+
+
+
 });
+
